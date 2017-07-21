89d27baaa9f30a6b288ad5a32b139bf20cdbe6dd
seongbaeSon <seongbae.son@gmail.com>
2017-07-21 20:49:19 +0900

    third commit

--- test_program.c
+++ test_program.c
@@ -18,30 +18,30 @@ struct timespec T1, T2;
 void withSync(char ch, int file_num, int record_size) {
 	int idx = 0;
 	int g = 0;
-    int f[10000];
-    char *buf = NULL;
-    char filename[20];
+	int f[10000];
+	char *buf = NULL;
+	char filename[20];
 
 	buf = (char*) malloc(1024 * 64);
 	memset(buf, ch, 1024 * 64);
 
-    for(idx = 0; idx < file_num; idx++) {
+	for(idx = 0; idx < file_num; idx++) {
 		sprintf(filename, "f_%d.txt", idx);
 
-        f[idx] = open(filename, O_RDWR | O_CREAT);
-        lseek (f[idx], 0, SEEK_END);
+		f[idx] = open(filename, O_RDWR | O_CREAT);
+		lseek (f[idx], 0, SEEK_END);
 
-        write(f[idx], buf, (record_size-1) * KB);
-        write(f[idx], buf, (1 * KB)-1);
-        write(f[idx], "\n", 1); 
+		write(f[idx], buf, (record_size-1) * KB);
+		write(f[idx], buf, (1 * KB)-1);
+		write(f[idx], "\n", 1); 
 
-        if(idx == file_num-1) {
-	        clock_gettime(CLOCK_MONOTONIC, &T1);
-		    fsync(f[idx]);
-            clock_gettime(CLOCK_MONOTONIC, &T2);
-        }
-        close (f[idx]);
-    }
+		if(idx == file_num-1) {
+			clock_gettime(CLOCK_MONOTONIC, &T1);
+			fsync(f[idx]);
+			clock_gettime(CLOCK_MONOTONIC, &T2);
+		}
+		close (f[idx]);
+	}
 }
 
 int main(int argc, const char * argv[])
@@ -52,7 +52,7 @@ int main(int argc, const char * argv[])
 
 	file_num = atoi(argv[1]);
 	record_size = atoi(argv[2]);
-    memcpy(&ch, argv[3], 1);
+	memcpy(&ch, argv[3], 1);
 	
 	withSync(ch, file_num, record_size);
 	time = BILLION * (T2.tv_sec - T1.tv_sec) + T2.tv_nsec - T1.tv_nsec;
@@ -60,4 +60,3 @@ int main(int argc, const char * argv[])
 
 	return 0;
 }
-
